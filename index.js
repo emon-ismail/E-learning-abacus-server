61dfcc74fdd8c4b18d85f762057af7ef7e9dea26
Tonmoy-Roy <emonismail69@gmail.com>
2022-10-26 20:31:25 +0600

    index all updated

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
 
 
-const express = require('express');
-const app =express();
-const cors =require('cors');
-const port =process.env.PORT || 5000;
+const express = require('express')
+const app = express()
+const port = 5000;
+const cors = require('cors');
 
-app.use(cors())
-const courses= require('./data/courses.json')
+app.use(cors());
 
-const course=require('./data/course.json')
+const topics= require('./data/topics.json');
+const topicsDetails = require('./data/topicDetails.json');
 
-app.get('/',(req,res)=>{
-    res.send("Course Api Running")
-});
-app.get('/course-courses', (req, res) => {
-    res.send(courses)
-});
-app.get('/single-course/:id',(req,res)=>{
-    const id=req.params.id;
-    if(id==='08'){
-res.send(course)
-    }
-    else{
-        const singleCourseCourse=course.filter(n=>n.course_id ===id);
-        res.send(singleCourseCourse)
-    }
-    
 
-})
-app.get('/course', (req, res) =>{
-    res.send(course);
-});
-app.get('/course/:id',(req,res)=>{
-    const id=req.params.id;
-   const salectedCourse= course.find(n=>n._id == id)
-   res.send(salectedCourse)
-});
-app.get('/course/:id',(req,res)=>{
-    res.send(course)
-})
-app.listen(port,()=>{
-    console.log("Abacus server running on",port)
-})
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+app.get('/topics', (req, res) => {
+    res.send(topics);
+  })
+
+app.get('/course/:id', (req, res) => {
+
+    const  id = req.params.id;
+    const all_course = topicsDetails.filter(n => n.category_id === id);
+    res.send(all_course );
+  })
+
+
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
